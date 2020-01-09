09316e661954ed4c20e75fb3e19f10d6cb66c191
Conal RYAN <conaleryan@gmail.com>
2020-01-09 08:51:00 -0500

    chore: merge lerna-hospital-sdk into typescript/build-tools

---
+++ build-tools/lerna-hospital-sdk/README.md
@@ -0,0 +1,5 @@
+# Learna da lerna a
+
+https://blog.logrocket.com/setting-up-a-monorepo-with-lerna-for-a-typescript-project-b6a81fe8e4f8
+
+https://www.npmjs.com/settings/lerna-hospital-sdk/packages
---
+++ build-tools/lerna-hospital-sdk/integration/src/index.ts
@@ -0,0 +1,7 @@
+import { Doctor } from "@lerna-hospital-sdk/doctor";
+import { Patient } from "@lerna-hospital-sdk/patient";
+import { Scheduler } from "@lerna-hospital-sdk/scheduler";
+
+console.log(new Doctor());
+console.log(new Scheduler());
+console.log(new Patient());
