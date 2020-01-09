177c419572fadaf808640dc82cb5dfaa8319fd60
Conal RYAN <conaleryan@gmail.com>
2020-01-09 08:37:03 -0500

    chore: merge ngrx into typescript/frameworks/ngrx

---
+++ frameworks/ngrx/apps-libs/projects/counter/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of counter
+ */
+
+export * from './lib/counter.actions';
+export * from './lib/counter.component';
+export * from './lib/counter.module';
+export * from './lib/counter.reducer';
