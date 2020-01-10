f59f800274c4cd226fcea273752b8c6e4c3af139
Conal RYAN <conaleryan@gmail.com>
2020-01-10 17:16:16 -0500

    createGraph function

--- build-tools/webpack/build-your-own-webpack/bundler.js
+++ build-tools/webpack/build-your-own-webpack/bundler.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
+const path = require('path');
 const babelParser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 
 let COUNTER = 0;
 
+/**
+ * fileName is the path relative to bundler.js
+ */
 function createAsset(fileName) {
+  // Read file content
   const content = fs.readFileSync(fileName, 'utf-8');
+  // Transform file content to AST
   const ast = babelParser.parse(content, {
     sourceType: 'module'
   });
-  
+  // Traverse AST and collect imports
   const dependencies = [];
-
   traverse(ast, {
     ImportDeclaration: ({node}) => {
       dependencies.push(node.source.value);
     }
   });
-
+  // Return an object representing the module dependencies
   const id = COUNTER++;
-  
   return {
     id,
     fileName,
@@ -27,6 +31,25 @@ function createAsset(fileName) {
   };
 }
 
-const mainAsset = createAsset('./example/entry.js');
+function createGraph(pathToEntryJs) {
+  const mainAsset = createAsset(pathToEntryJs);
+  // Loop and push to queue traversing through all modules
+  const queue = [mainAsset];
+  for(const asset of queue) {
+    const dirname = path.dirname(asset.fileName);
 
-console.log(mainAsset);
+    asset.mapping = {};
+
+    asset.dependencies.forEach(relativePath => {
+      const absolutePath = path.join(dirname, relativePath);
+      const childAsset = createAsset(absolutePath);
+      asset.mapping[relativePath] = childAsset.id;
+      queue.push(childAsset);
+    });
+  }
+  return queue;
+}
+
+const graph = createGraph('./example/entry.js');
+
+console.log(graph);
