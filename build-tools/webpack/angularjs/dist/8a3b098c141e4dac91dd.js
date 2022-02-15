1f398aa462dd0a9d4e84392fb995a782de6e12ed
Conal Ryan <corya@akamai.com>
2022-02-15 11:59:47 -0500

    feat (webpack) angularjs-menu-component

--- build-tools/webpack/angularjs/dist/8a3b098c141e4dac91dd.js
+++
@@ -1,1 +0,0 @@
-import angular from"angular";import"./app.css";angular.module("appjs",[]).controller("MainCtrl",(function(){this.message="Hello world from AngularJS"}));
---
+++ build-tools/webpack/angularjs/src/menu/index.js
@@ -0,0 +1,7 @@
+import * as angular from 'angular';
+
+import { menuHeader } from './menu-header';
+
+export default angular
+  .module('appjs.menu', [])
+  .component('appjsMenuHeader', menuHeader);
