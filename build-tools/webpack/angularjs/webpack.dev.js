6a7f45180e799831080e787dfe1b9cc40de13152
Conal Ryan <corya@akamai.com>
2022-02-15 12:16:30 -0500

    feat (webpack) angularjs split modules into chuncks

--- build-tools/webpack/angularjs/webpack.dev.js
+++ build-tools/webpack/angularjs/webpack.dev.js
@@ -1,18 +1,18 @@
 const path = require("path");
 const common = require("./webpack.common");
-const { merge } = require('webpack-merge') // webpack-merge v5 (and later) use named export
+const { merge } = require("webpack-merge"); // webpack-merge v5 (and later) use named export
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = merge(common, {
+const main = merge(common, {
   mode: "development",
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    })
+      template: "./src/index.html",
+    }),
   ],
   module: {
     rules: [
@@ -22,8 +22,21 @@ module.exports = merge(common, {
           "style-loader", // 3. Inject styles into DOM
           "css-loader", // 2. Turns css into commonjs
           // "sass-loader" // 1. Turns sass into css
-        ]
-      }
-    ]
-  }
+        ],
+      },
+    ],
+  },
 });
+
+const menu = merge(main, {
+  name: "menu",
+  entry: {
+    menu: ["./src/menu/menu-header.css", "./src/menu/menu-header.js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+});
+
+module.exports = [main, menu];
