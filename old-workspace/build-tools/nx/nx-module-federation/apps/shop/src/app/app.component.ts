29706f8a9e835deb53ed10513d99080b63681a46
Conal Ryan <corya@akamai.com>
2023-05-05 13:14:45 -0400

    cleanup

--- old-workspace/build-tools/nx/nx-module-federation/apps/shop/src/app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'nx-module-federation-root',
-  template: '<router-outlet></router-outlet>',
-})
-export class AppComponent {}
--- old-workspace/build-tools/nx/nx-setup.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-
-npm i -g @angular/cli @nrwl/cli nx
-
-npm i -g @angular-devkit/schematics-cli @nrwl/schematics @ngrx/schematics
-
-
